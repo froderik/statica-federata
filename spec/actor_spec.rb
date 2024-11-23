f628ff6488199fba462f529e9102de8e1e4fdf10
Fredrik Rubensson <fredrik@mazin.cc>
2024-11-23 14:12:35 +0100

    yay there can be nesting

--- spec/actor_spec.rb
+++ spec/actor_spec.rb
@@ -5,61 +5,63 @@ require 'json'
 require 'securerandom'
 require './spec/shared-test-module.rb'
 
-describe 'getting an actor' do
+describe 'an actor' do
   include SharedTest
 
-  before :each do
-    @actor_name = 'mazin'
-    get "/users/#{@actor_name}"
-    print last_response.status
-    expect(last_response.status).to be 200
-    body = last_response.body
-    @json = JSON.parse body
-  end
+  describe 'getting an actor' do
 
-  it 'has an id' do
-    expect( @json['id'] ).to eq "https://mastodon.mazin.cc/users/#{@actor_name}"
-  end
+    before :each do
+      @actor_name = 'mazin'
+      get "/users/#{@actor_name}"
+      print last_response.status
+      expect(last_response.status).to be 200
+      body = last_response.body
+      @json = JSON.parse body
+    end
 
-  it 'has a type' do
-    expect( @json['type'] ).to eq 'weblog'
-  end
+    it 'has an id' do
+      expect( @json['id'] ).to eq "https://mastodon.mazin.cc/users/#{@actor_name}"
+    end
 
-  ['following', 'followers', 'inbox', 'outbox'].each do |action|
-    it "has #{action}" do
-      expect( @json[action] ).to eq "https://mastodon.mazin.cc/users/#{@actor_name}/#{action}"
+    it 'has a type' do
+      expect( @json['type'] ).to eq 'weblog'
+    end
+
+    ['following', 'followers', 'inbox', 'outbox'].each do |action|
+      it "has #{action}" do
+        expect( @json[action] ).to eq "https://mastodon.mazin.cc/users/#{@actor_name}/#{action}"
+      end
     end
   end
-end
 
-describe 'following an actor' do
-  include SharedTest
+  describe 'following an actor' do
 
-  before :each do
-    @followee_actor_name = 'followee'
-    @follower_actor_name = 'follower'
-  end
+    before :each do
+      @followee_actor_name = 'followee'
+      @follower_actor_name = 'follower'
+    end
 
-  def domain
-    "https://mastodon.mazin.cc"
-  end
+    def domain
+      "https://mastodon.mazin.cc"
+    end
 
-  def identifier_from actor_name
-    "#{domain}/users/#{actor_name}"
-  end
+    def identifier_from actor_name
+      "#{domain}/users/#{actor_name}"
+    end
 
-  def random_activity_id
-    "#{domain}/#{SecureRandom.uuid}"
-  end
+    def random_activity_id
+      "#{domain}/#{SecureRandom.uuid}"
+    end
 
-  it 'follows' do
-    body = {
-      'type' => 'Follow',
-      'id' => random_activity_id,
-      'actor' => identifier_from(@follower_actor_name),
-      'object' => identifier_from(@followee_actor_name)
-    }
-    inbox_url = get( "/users/#{@followee_actor_name}" ).body['inbox']
-    post inbox_url, body
+    it 'follows' do
+      body = {
+        'type' => 'Follow',
+        'id' => random_activity_id,
+        'actor' => identifier_from(@follower_actor_name),
+        'object' => identifier_from(@followee_actor_name)
+      }
+      inbox_url = get( "/users/#{@followee_actor_name}" ).body['inbox']
+      post inbox_url, body
+    end
   end
 end
